f06d6088adc58b441411dd100ab27b16b253cb0a
Maria <maria.altyeva@gmail.com>
2020-01-06 14:44:58 +0800

    global id

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -7,8 +7,14 @@ class Posts::LikesController < ApplicationController
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    current_user.users_posts.find_by(post: post).destroy!
-    redirect_to post, notice: "unliked post!"
+    # post = Post.find(params[:post_id])
+    # current_user.users_posts.find_by(post: post).destroy!
+    # redirect_to post, notice: "unliked post!"
+
+    # users_post = UsersPost.find(params[:id])
+    users_post = GlobalID::Locator.locate(params[:id])
+    users_post.destroy!
+
+    redirect_to post_path(users_post.post_id), notice: "unliked post"
   end
 end
