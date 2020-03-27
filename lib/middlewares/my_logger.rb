f28d13d4b1e93ab2081e0b3fcc94db0d526483c6
Maria <maria.altyeva@gmail.com>
2020-03-27 15:01:51 +0800

    middleware testing

--- lib/middlewares/my_logger.rb
+++ lib/middlewares/my_logger.rb
@@ -4,8 +4,17 @@ class MyLogger
   end
 
   def call(env)
+    start_time = Time.now
     status, headers, body = @app.call(env)
-    puts "MyLogger::#{Time.now}"
+    # puts "MyLogger::#{Time.now}"
+    end_time = Time.now
+
+    options = {
+      expires: 1.day.from_now,
+      value: 'my_value',
+      path: '/'
+    }
+    Rack::Utils.set_cookie_header!(headers, 'my_cookie', options)
     [status, headers, body]
   end
 end
